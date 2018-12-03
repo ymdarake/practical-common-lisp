764ef864ed3d2e7750bf6a68a6a5702cb4407fdc
Takumi.Sakamoto <t.sakamoto.ouen@gmail.com>
2018-12-03 23:58:54 +0900

    [update]chap16 oop: defclass

--- basics/chapter16-oop-generic.lisp
+++ basics/chapter16-oop-generic.lisp
@@ -1,28 +1,46 @@
-(defgeneric withdraw (account amount)
-  (:documentation "withdraw 'amount' from 'account'. If the current balance is lower than the amount, then notify error."))
+;; (defgeneric withdraw (account amount)
+;;   (:documentation "withdraw 'amount' from 'account'. If the current balance is lower than the amount, then notify error."))
 
-(defmethod withdraw ((account bank-account) amount)
-  (when (< (balance account) amount)
-    (error "Account overdrawn."))
-  (decf (balance account) amount))
+;; (defmethod withdraw ((account bank-account) amount)
+;;   (when (< (balance account) amount)
+;;     (error "Account overdrawn."))
+;;   (decf (balance account) amount))
 
-(defmethod withdraw ((account checking-account) amount)
-  (let ((overdraft (- amount (balance account))))
-    (when (plusp overdraft)
-      (withdraw (overdraft-account account) overdraft)
-      (incf (balance account) overdraft)))
-  (call-next-method))
+;; (defmethod withdraw ((account checking-account) amount)
+;;   (let ((overdraft (- amount (balance account))))
+;;     (when (plusp overdraft)
+;;       (withdraw (overdraft-account account) overdraft)
+;;       (incf (balance account) overdraft)))
+;;   (call-next-method))
 
-(defmethod withdraw ((proxy proxy-account) amount)
-  (withdraw (proxied-account proxy) amount))
+;; (defmethod withdraw ((proxy proxy-account) amount)
+;;   (withdraw (proxied-account proxy) amount))
 
 
-;; before, after, around
+;; ;; before, after, around
 
-(defmethod withdraw :before ((account checking-account) amount)
-  (let ((overdraft (- amount (balance account))))
-    (when (plusp overdraft)
-      (withdraw (overdraft-account account) overdraft)
-      (incf (balance account) overdraft))))
+;; (defmethod withdraw :before ((account checking-account) amount)
+;;   (let ((overdraft (- amount (balance account))))
+;;     (when (plusp overdraft)
+;;       (withdraw (overdraft-account account) overdraft)
+;;       (incf (balance account) overdraft))))
 
+(defvar *account-numbers* 0)
+
+(defclass bank-account ()
+  ((customer-name
+    :initarg :customer-name
+    :initform (error "Must supply a customer name."))
+   (balance
+    :initarg :balance
+    :initform 0)
+   (account-number
+    :initform (incf *account-numbers*))))
+
+; (defparameter *account* (make-instance 'bank-account))
+
+;; (setf (slot-value *account* 'customer-name) "John Doe")
+;; (setf (slot-value *account* 'balance) 1000)
+;; (slot-value *account* 'customer-name)
+;; (slot-value *account* 'balance)
 
